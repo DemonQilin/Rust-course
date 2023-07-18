d61169e1cc21199fb7464764bcb3aa1dc6eb1e19
DemonQilin <velezloaizaesteban9012015@gmail.com>
2023-07-17 19:37:51 -0500

    add trait object class

--- intermediate/02_traits/src/main.rs
+++ intermediate/02_traits/src/main.rs
@@ -31,10 +31,26 @@
     Se puede usar la sintaxis "impl <TraitName>" para indicar que el retorno de una función implementa cierto(s) Trait(s) pero sin que el codigo que llama conozca el tipo concreto que se retorna.
 
     Es importante tener en cuenta que la función debe unicamente retornar un tipo, es decir, no puedo tener un condicional que retorne un tipo o el otro a pesar de que implementen el trait.
+
+    SUPERTRAITS
+    Son traits que son relacionados con otros en su implementación. Mas espcificamente, en la definición de una Trait se puede especificar que al implementarse debe implementar otro(s) trait(s) y estos traits son los llamados super trait.
+
+    Para definir un super Trait en la definición de otro trait despues de su nombre se usando dos puntos y el nombre del super trait que se quiere relacionar.
+
+        trait SomeTrait: SuperTrait + OtherSuperTrait {}
+
+    TRAIT OBJECTS
+    Permiten tratar diferentes tipos que implementan un trait de forma uniforme, donde solo se tiene acceso a los metodos definidos por el trait. Estos tipos no pueden ser reemplazados por tipos concretos en tiempo de compilación por lo que requiren necesitan ser referencias o punteros al Heap para poder utilizar memoria variable. Se logran así:
+
+        - Con referencia: &dyn Trait
+        - Con puntero: Box<dyn Trait>
+
+    Permiten flexibilidad en el codigo al tratarlos mediante un interfaz compartida sin conocer su implementación subyacente en tiempo de compilación. Son muy usados para crear listas de tipos que tienen que implementan un Trait comun, debido a que solo permiten un tipo concreto de elementos.
 */
 
 // Definimos un trait con el proposito de que cualquier que lo implemente tenga la capacidad de parquear
-trait Park {
+// Hacemos que Paint sea un supertrait para Park, es decir, cualquier tipo que implemente Park tambien debera implementar Paint
+trait Park: Paint {
     fn park(&self);
 }
 
@@ -97,12 +113,20 @@ fn main() {
         },
     };
     let house = House {};
-    // El codigo cliente no conoce el tipo concreto pero sabe que puede usar todos los metodos del Trati Paint
-    let paintable_object = create_paintable_object();
+    // El codigo cliente no conoce el tipo concreto pero sabe que puede usar todos los metodos del Trat Paint
+    // Devuelve un tipo que no se conoce en tiempo de compilación pero que se sabe implementa el Trati Paint
+    let paintable_object = create_paintable_object(false);
+
+    // Podemos definir un vector que almacena items de distintos tipos que implementan el trait Paint mediante trait objects
+    let paintable_objects: Vec<&dyn Paint> = vec![&car, &house];
+
+    for object in paintable_objects {
+        object.paint("Blue".to_owned());
+    }
 
     paint_red(&car);
     paint_red(&house);
-    paint_red(&paintable_object);
+    paint_red(paintable_object.as_ref());
 
     // Solo el tipo Car implementa los trait Paint y Park
     paint_vehicle_red(&car);
@@ -111,7 +135,8 @@ fn main() {
 }
 
 // Define una función que recibe cualquier tipo que tenga la funcionalidad de pintarse usando la sintaxis Trait Bound
-fn paint_red<T: Paint>(object: &T) {
+// Debido a que se pasa un tipo despachado en tiempo de ejecución se debe definir como una referencia dinamica
+fn paint_red(object: &dyn Paint) {
     object.paint("red".to_owned());
 }
 
@@ -123,12 +148,25 @@ fn paint_red<T: Paint>(object: &T) {
 // Define la funcionalidad de pintar de rojo cualquier tipo pueda ser pintando y parqueado la clausula "where".
 fn paint_vehicle_red<T>(object: &T)
 where
-    T: Paint + Park,
+    // Ahora que Park implica Pain no es necesario especificar ambos, con Pak basta
+    // T: Paint + Park,
+    T: Park,
 {
     object.paint("red".to_owned())
 }
 
 // Define una función que crea objetos que pueden ser pintados
-fn create_paintable_object() -> impl Paint {
-    House {}
+// Se implementa Trait Object para retornar distintos tipos de objeto pintables donde en tiempo de compilación no es posible saber cual concretamente.
+fn create_paintable_object(vehicle: bool) -> Box<dyn Paint> {
+    if vehicle {
+        Box::new(Car {
+            info: VehicleInfo {
+                make: "BMW".to_owned(),
+                model: "Deportivo".to_owned(),
+                year: 2023,
+            },
+        })
+    } else {
+        Box::new(House {})
+    }
 }
